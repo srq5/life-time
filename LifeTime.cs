69539702ba934c16b5ec1be70e3196a50d83c60d
Steven <stevenesammon@hotmail.com>
2018-02-05 23:52:42 -0500

    adds exe and cs file

---
+++ LifeTime.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LifeTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Please enter your birthday in the format MM/DD/YYYY");
+            Console.ResetColor();
+            
+            
+            //Here we will collect the user birthday input.
+            DateTime userBirthday = DateTime.Parse(Console.ReadLine());
+            //We subtract the birthday from the current day.
+            TimeSpan myAge = DateTime.Now.Subtract(userBirthday);
+
+            //Display results to the end user.
+            Console.WriteLine("You have been alive for...");
+            Console.WriteLine(String.Format("{0:N}", myAge.TotalDays) + " days.");
+            Console.WriteLine(String.Format("{0:N}", myAge.TotalHours) + " hours.");
+            Console.WriteLine(String.Format("{0:N}", myAge.TotalMinutes) + " minutes.");
+            Console.WriteLine(String.Format("{0:N}", myAge.TotalSeconds) + " seconds.");
+
+            Console.ReadLine();
+
+        }
+    }
+}
